09d9e877e47c1c5d2ca7d898e7c8ca8573f46512
Kai Klostermann <kai@ungra.dev>
2023-02-18 16:39:43 +0100

    Should work now

--- main.js
+++ main.js
@@ -1,81 +1,56 @@
 const options = new Map();
 
 window.addEventListener("DOMContentLoaded", function () {
-  initThemeSwitcher();
   initEventListeners();
-  // Settings default values
-  setLocalStorageItem(
-    "lightTheme",
-    !matchMedia("(prefers-color-scheme: dark)").matches
-  );
   initSettingsButton();
 });
 
+const settings = new Map();
+settings.set("theme", ["dark", "light"]);
+settings.set("motion", ["normal", "reduced"]);
+
 function initEventListeners() {
-  document.getElementById('theme-switcher-option-system').addEventListener('change', () => {
-    localStorage.removeItem('theme');
-    document.body.removeAttribute('data-theme');
-  });
-  document.getElementById('theme-switcher-option-light').addEventListener('change', () => {
-    localStorage.setItem('theme', 'light');
-    document.body.setAttribute('data-theme', 'light');
-  });
-  document.getElementById('theme-switcher-option-dark').addEventListener('change', () => {
-    localStorage.setItem('theme', 'dark');
-    document.body.setAttribute('data-theme', 'dark');
-  });
+  settings.forEach(
+    (optionIdentifiers, groupIdentifier) =>
+      initEventListener(groupIdentifier) ||
+      optionIdentifiers.forEach((optionIdentifier) =>
+        initEventListener(groupIdentifier, optionIdentifier)
+      )
+  );
 }
 
-function initThemeSwitcher() {
-  const entry = localStorage.getItem('theme');
-  if (entry === null) {
-    document.getElementById('theme-switcher-option-system').checked = true;
-    return;
-  }
-  if (entry === 'light') {
-    document.getElementById('theme-switcher-option-light').checked = true;
-    document.body.setAttribute('data-theme', 'light');
-    return
-  } 
-  document.getElementById('theme-switcher-option-dark').checked = true;
-  document.body.setAttribute('data-theme', 'dark');
-  
-  /* const themeSwitcherCheckBox = document.querySelector(
-    ".checkbox--theme-switcher"
+function initEventListener(groupIdentifier, option = "system") {
+  console.log(`${groupIdentifier}-switcher-option-${option}`);
+  const radioButtonElement = document.getElementById(
+    `${groupIdentifier}-switcher-option-${option}`
   );
-  options.set("lightTheme", themeSwitcherCheckBox);
-  themeSwitcherCheckBox.addEventListener("click", (clickEvent) => {
-    clickEvent.preventDefault();
-    console.log(clickEvent.target);
-    changeLocalStorageItem("lightTheme", clickEvent.target.checked);
-  }); */
+  radioButtonElement.addEventListener("change", () =>
+    setAttribute(groupIdentifier, option)
+  );
+  initButtonState(groupIdentifier, option, radioButtonElement);
+}
+
+function initButtonState(groupIdentifier, option, buttonSourceElement) {
+  const storageEntry = localStorage.getItem(groupIdentifier),
+    isActive =
+      (option === "system" && storageEntry === null) || storageEntry === option;
+
+  if (isActive) {
+    setAttribute(groupIdentifier, option);
+  }
+  buttonSourceElement.checked = isActive;
+}
+
+function setAttribute(name, value) {
+  document.body.setAttribute(`data-${name}`, value);
+  localStorage.setItem(name, value);
 }
 
 function initSettingsButton() {
-  const settingsButton = document.querySelector(".settings"),
+  const settingsButton = document.querySelector(".settings-toggle"),
     settingsFlyIn = document.querySelector("aside");
 
   settingsButton.addEventListener("click", () =>
     settingsFlyIn.classList.toggle("open")
   );
 }
-
-function setLocalStorageItem(itemName, defaultValue) {
-  let currentValue = localStorage.getItem(itemName);
-  if (currentValue === null) {
-    changeLocalStorageItem(itemName, defaultValue);
-  }
-}
-
-function changeLocalStorageItem(key, value) {
-  console.log(key, value);
-  localStorage.setItem(key, value);
-  // console.log(options.get(key));
-  // options.get(key).checked = value;
-}
-
-/* setLocalStorageItem(
-  "noAnimations",
-  matchMedia("(prefers-reduced-motion)").matches
-);
-setLocalStorageItem("nerdLevel", 0); */
