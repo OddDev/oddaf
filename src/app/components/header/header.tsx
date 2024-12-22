ec0ad9f4b13d7e903659a0d7eebe2c3f455727b9
Kai Klostermann <kai@ungra.dev>
2024-12-22 11:49:40 +0100

    ✨ feat: added motion preferences

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,6 +1,7 @@
 import Link from "../link/link";
 import styles from "./header.module.css";
 import Logo from "../logo/logo";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export default function Header() {
   return (
@@ -10,7 +11,39 @@ export default function Header() {
         OddDev
       </div>
       <section className={styles.banner}>
-        <h1>Foobar</h1>
+        <fieldset className={styles.radioGroupWrapper}>
+          <legend className={styles.radioGroupLegend}>Motion Preferences</legend>
+          <RadioGroup.Root
+            className={styles.RadioGroupRoot}
+            defaultValue="system"
+            aria-label="Motion preferences"
+          >
+            <div className={styles.RadioGroupItemWrapper}>
+              <RadioGroup.Item className={styles.RadioGroupItem} value="system" id="option-system">
+                <RadioGroup.Indicator className={styles.RadioGroupIndicator} />
+              </RadioGroup.Item>
+              <label className={styles.Label} htmlFor="option-system">
+                System
+              </label>
+            </div>
+            <div className={styles.RadioGroupItemWrapper}>
+              <RadioGroup.Item className={styles.RadioGroupItem} value="reduced" id="option-reduced">
+                <RadioGroup.Indicator className={styles.RadioGroupIndicator} />
+              </RadioGroup.Item>
+              <label className={styles.Label} htmlFor="option-reduced">
+                Reduced
+              </label>
+            </div>
+            <div className={styles.RadioGroupItemWrapper}> 
+              <RadioGroup.Item className={styles.RadioGroupItem} value="normal" id="option-normal">
+                <RadioGroup.Indicator className={styles.RadioGroupIndicator} />
+              </RadioGroup.Item>
+              <label className={styles.Label} htmlFor="option-normal">
+                Normal
+              </label>
+            </div>
+          </RadioGroup.Root>
+        </fieldset>
       </section>
       <Link>
         Message Me ↣
